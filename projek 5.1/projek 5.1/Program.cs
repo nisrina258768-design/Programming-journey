b4bbe6362abb16662840691b9e68b3c38c54247b
nisrina258768-design <nisrina258768@student.smkn1kandeman.sch.id>
2025-10-21 10:45:23 +0700

    Add files via upload

---
+++ projek 5.1/projek 5.1/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace projek_5._1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Membuat array dengan 5 elemen tipe data string
+            string[] siswa = new string[5];
+
+            //Mengisi elemen Array
+            siswa[0] = "Nisrina";
+            siswa[1] = "Agda";
+            siswa[2] = "Ayu";
+            siswa[3] = "Intan";
+            siswa[4] = "Kirana";
+
+            //Menampilkan isi Array tanpa perulangan
+            Console.WriteLine("Daftar nama siswa: ");
+            Console.WriteLine("Siswa ke-1: " + siswa[0]);
+            Console.WriteLine("Siswa ke-2: " + siswa[1]);
+            Console.WriteLine("Siswa ke-3: " + siswa[2]);
+            Console.WriteLine("Siswa ke-4: " + siswa[3]);
+            Console.WriteLine("Siswa ke-5: " + siswa[4]);
+    
+        }
+    }
+}
---
+++ projek 5.2/projek 5.2/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace projek_5._2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Membuat Array integer dengan 5 elemen
+            int[] angka = new int[5];
+
+            //Input elemen Array dari keyboard
+            Console.Write("Masukkan nilai ke-1: ");
+            angka[0] = Convert.ToInt32(Console.ReadLine());
+
+            Console.Write("Masukkan nilai ke-2: ");
+            angka[1] = Convert.ToInt32(Console.ReadLine());
+
+            Console.Write("Masukkan nilai ke-3: ");
+            angka[2] = Convert.ToInt32(Console.ReadLine());
+
+            Console.Write("Masukkan nilai ke-4: ");
+            angka[3] = Convert.ToInt32(Console.ReadLine());
+
+            Console.Write("Masukkan nilai ke-5: ");
+            angka[4] = Convert.ToInt32(Console.ReadLine());
+
+            //Menampilkan isi Array tanpa perulangan
+            Console.WriteLine("\nData yang anda masukkan: ");
+            Console.WriteLine("Nilai ke-1: " + angka[0]);
+            Console.WriteLine("Nilai ke-2: " + angka[1]);
+            Console.WriteLine("Nilai ke-3: " + angka[2]);
+            Console.WriteLine("Nilai ke-4: " + angka[3]);
+            Console.WriteLine("Nilai ke-5: " + angka[4]);
+
+        }
+    }
+}
